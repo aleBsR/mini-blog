07135da4ca82b56cdf7b0b757f20d23666fae6ed
aleBsR <alejandrosandoval211@gmail.com>
2025-01-21 14:51:37 -0300

    agregando todas las funciones para mostrar los Post de acuerdo a las fechas ingresadas

--- blog.py
+++ blog.py
@@ -27,3 +27,36 @@ class Blog:
         for post in self.lista:
             if post.getFecha() >= fecha_ingresada:
                 print(post)
+                
+    def mostrarHasta(self):
+        print("Ingrese fecha:")
+        anio = input("anio: ")
+        mes = input("mes: ")
+        dia = input("dia: ")
+        fecha_ingresada = Fecha(f"{anio}-{mes}-{dia}").fecha
+
+        print(f"Post de fechas ingresadas hasta {fecha_ingresada.strftime('%d/%m/%Y')}:")
+
+        for post in self.lista:
+            if post.getFecha() <= fecha_ingresada:
+                print(post)
+    
+    def mostrarDesdeHasta(self):
+        print("Ingrese fecha desde la que quiere buscar:")
+        anio = input("anio: ")
+        mes = input("mes: ")
+        dia = input("dia: ")
+        fecha_ingresada = Fecha(f"{anio}-{mes}-{dia}").fecha
+        
+        print("Ingrese fecha hasta la que quiere buscar:")
+        anio2 = input("anio: ")
+        mes2 = input("mes: ")
+        dia2 = input("dia: ")
+        segunda_fecha = Fecha(f"{anio2}-{mes2}-{dia2}").fecha
+
+        print(f"Post de fechas ingresadas a partir de {fecha_ingresada.strftime('%d/%m/%Y')} hasta {segunda_fecha.strftime('%d/%m/%Y')}:")
+
+        for post in self.lista:
+            if post.getFecha() >= fecha_ingresada:
+                if post.getFecha() <= segunda_fecha:
+                    print(post)
